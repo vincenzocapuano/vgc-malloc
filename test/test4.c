36569d847e5da72e30aec4b60c1c49aed95d816b
Vincenzo Capuano <vcapuano@vgcsoft.org>
2023-07-06 14:33:19 +0200

    Testing mprotect() maximum number of calls

---
+++ test/test4.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/mman.h>
+
+// Test how many mprotect we can call, it is regulated by parameter /proc/sys/vm/max_map_count
+
+#define PAGESIZE 4096
+
+int main(void)
+{
+	for (int i = 1; i < 750000; i++) {
+		char *buffer = memalign(PAGESIZE, PAGESIZE * 4);
+		int r = mprotect(buffer, PAGESIZE, PROT_NONE);
+		if (r == -1) {
+			printf("Stopped at: %i\n", i);
+			return 1;
+		}
+	}
+
+	return 0;
+}
