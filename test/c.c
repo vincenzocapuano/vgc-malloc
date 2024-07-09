b3f3f5d37fe25bf96c9dab46ad154ea363d9f7ca
Vincenzo Capuano <vcapuano@vgcsoft.org>
2024-07-09 13:28:06 +0200

    To remove

--- test/c.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-	int a = 4 * 1024;
-	int A = (4 * 1024) - 1;
-	int b = a * 3 - 5 + 0x10000;
-	printf("b: 0x%x - c: 0x%x - d: 0x%x %d\n", b,      b & ~A,       b & A,       a - b & A);
-}
